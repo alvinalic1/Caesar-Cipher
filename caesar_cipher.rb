55b43ecd0e3642e2a4c9c0d13b3589a9b545672d
Alvin Alic <alvin.alic@gmail.com>
2024-09-26 17:58:56 -0500

    Create method with the variables that will be used

---
+++ caesar_cipher.rb
@@ -0,0 +1,14 @@
+def ceasar_cipher(string, shift_factor)
+  ascii_values = string.codepoints
+  new_ascii_values = []
+  upper_case_a = 65
+  lower_case_a = 97
+  upper_case_z = 90
+  lower_case_z = 122
+  ascii_values.each do |ascii|
+    if (ascii >= 65 && ascii <= 90) || (ascii >= 97 )
+    
+  end
+end
+
+puts ceasar_cipher("Alvin", 5)
